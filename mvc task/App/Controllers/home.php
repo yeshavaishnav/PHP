7df4685edbf9ff7ea9facc7e4fac929324f426a3
Yesha Vaishnav <yeshavaishnav@gmail.com>
2020-02-19 14:14:43 +0530

    mvc ecommerce complete

--- mvc task/App/Controllers/home.php
+++ mvc task/App/Controllers/home.php
@@ -10,11 +10,11 @@ class Home
 
     public function index()
     {
-        $header = Database::getDistinct('categories','parent');
+        $header = Database::getDistinct('categories', 'parent');
         $footer = Database::getAll('cms_pages');
         $content = Database::getAll('cms_pages', "WHERE urlKey = 'homepage'");
-        View::renderTemplate('cms/page.html', ['header' => $header, 'content' => $content, 'footer' => $footer]);
-        
+        $categories = Database::getAll('categories');
+        View::renderTemplate('cms/page.html', ['header' => $header, 'content' => $content, 'footer' => $footer, 'categories' => $categories]);
 
     }
 }
--- mvc task/App/Controllers/show.php
+++ mvc task/App/Controllers/show.php
@@ -13,15 +13,8 @@ class Show
         $header = Database::getDistinct('categories', 'parent');
         $footer = Database::getAll('cms_pages');
         $products = Database::getAll('products', "WHERE category = '" . $category . "'");
-        View::renderTemplate('showproducts.html', ['header' => $header, 'products' => $products, 'footer' => $footer]);
-    }
-    public function showCategories()
-    {
-        $category = $_GET['category'];
-        $header = Database::getDistinct('categories', 'parent');
-        $footer = Database::getAll('cms_pages');
-        $categories = Database::getAll('categories', "WHERE parent = '" . $category . "'");
-        View::renderTemplate('showCategories.html', ['header' => $header, 'categories' => $categories, 'footer' => $footer]);
+        $categories = Database::getAll('categories');
+        View::renderTemplate('showproducts.html', ['header' => $header, 'products' => $products, 'footer' => $footer, 'categories' => $categories]);
     }
     public function product()
     {
@@ -29,6 +22,7 @@ class Show
         $header = Database::getDistinct('categories', 'parent');
         $footer = Database::getAll('cms_pages');
         $products = Database::getAll('products', "WHERE productName = '" . $product . "'");
-        View::renderTemplate('productDisplay.html', ['header' => $header, 'products' => $products, 'footer' => $footer]);
+        $categories = Database::getAll('categories');
+        View::renderTemplate('productDisplay.html', ['header' => $header, 'products' => $products, 'footer' => $footer,'categories'=>$categories]);
     }
 }
